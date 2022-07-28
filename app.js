64428525848976e827ba1b1982cd46acfc967b22
HanNgoBa <bahanngo7@gmail.com>
2022-07-28 11:12:58 +0700

    HanNgoBa

---
+++ app.js
@@ -0,0 +1,39 @@
+let count = 0 ;
+const value = document.querySelector(".value");
+const btn = document.querySelectorAll(".btn");
+
+btn.forEach(function(btn)
+{
+    btn.addEventListener("click",function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease"))
+        {
+            count --; 
+        }
+
+        if(styles.contains("reset"))
+        {
+            count= 0;
+        }
+
+        if(styles.contains("increase"))
+        {
+            count ++;
+        }
+        
+        if(count > 0)
+        {
+            value.style.color="green";
+        }else if(count < 0){
+            value.style.color="red";
+        }else{
+            value.style.color="#222";
+
+        }
+
+        value.textContent = count;
+
+        
+    })
+})
+
